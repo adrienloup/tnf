5d9744b9bfefbb6f34521ca7051269dc514cc147
Adrien Loup <adloup@gmail.com>
2024-12-14 02:27:14 +0100

    feat(1) init tnf project

--- src/generic/components/article/Article.component.tsx
+++ src/generic/components/article/Article.component.tsx
@@ -1,5 +1,5 @@
 import { Article } from "@/src/generic/components/article/Article.type";
-import styles from "@/src/generic/components/Article/article.module.scss";
+import styles from "@/src/generic/components/article/article.module.scss";
 
 export const ArticleComponent = ({ children }: Article) => {
   return (
